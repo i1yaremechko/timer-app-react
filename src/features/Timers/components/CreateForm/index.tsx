4117a0130f0dd476ed752bbb428a629479ce0876
vnSasa <ol.yaremechko@gmail.com>
2026-07-24 16:33:07 +0300

    fixed default title

--- src/features/Timers/components/CreateForm/index.tsx
+++ src/features/Timers/components/CreateForm/index.tsx
@@ -5,12 +5,20 @@ interface CreateFormProps {
   onAddTimer: (title: string) => void;
 }
 
+const getDefaultTimerTitle = (): string => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  return `From ${hours}:${minutes}`;
+};
+
 export const CreateForm: React.FC<CreateFormProps> = ({ onAddTimer }) => {
   const [titleInput, setTitleInput] = useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const trimmedTitle = titleInput.trim();
+    const trimmedTitle = titleInput.trim() || getDefaultTimerTitle();
     if (!trimmedTitle) return;
 
     onAddTimer(trimmedTitle);
