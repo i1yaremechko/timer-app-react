58553e4786d2f2041e878f05f27a306979f38654
vnSasa <ol.yaremechko@gmail.com>
2026-07-24 16:29:23 +0300

    fixed sorting timers

--- src/features/Timers/hooks/useTimers.ts
+++ src/features/Timers/hooks/useTimers.ts
@@ -29,7 +29,7 @@ export const useTimers = () => {
       isRunning: true
     }
 
-    setTimers(prev => [...prev, newTimer])
+    setTimers(prev => [newTimer, ...prev])
   }
 
   const toggleTimer = (id: string) => {
